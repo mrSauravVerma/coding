04d1d35382b73182aff267d1f391b9931353b5c4
SAURAV VERMA <sauravverma9456@gmail.com>
2024-08-13 23:13:09 +0530

    13-8-24

--- javasript/22_DOM.js
+++ javasript/22_DOM.js
@@ -1,2 +1,5 @@
-let val = document.querySelector(div);
-let my = div.getAtrribute(ID);
+let div = document.querySelector("#id");  // Use '#' to select by id
+console.log(div);
+
+let id = div.getAttribute("id");  // Corrected the spelling of getAttribute
+console.log(id);
