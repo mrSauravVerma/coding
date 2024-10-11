644fd6e93d16b225503ede0428d154663243d228
SAURAV VERMA <sauravverma9456@gmail.com>
2024-10-11 17:52:16 +0530

    11/10/24

--- javasript/16_square_Arr_forEach.js
+++ javasript/16_square_Arr_forEach.js
@@ -1,6 +1,5 @@
-let Arr = [5,10,15,20,25];
-
-let fun = Arr.forEach(val => {
+let Arr = [5, 10, 15, 20, 25];
+let fun = Arr.forEach((val) => {   // forEach mathod dose not return anything
     console.log(val * val);
 });
 
@@ -8,10 +7,8 @@ let fun = Arr.forEach(val => {
 /////////////////   with map   //////////////////
 /////////////////////////////////////////////////
 console.log("with map mathod")
-let arr = [2,4,6,8,10];
+let arr = [2, 4, 6, 8, 10];
 
-let Fun = arr.map(val => {
-    return val * val ;
+let Fun = arr.map(val => {  // // map mathod return same size of array
+    return val * val;
 });
-
-console.log(Fun);
--- javasript/18_filterMethod.js
+++ javasript/18_filterMethod.js
@@ -1,9 +1,10 @@
-let arr = [1,2,3,4,5,6,7,8,9,10];
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// filter mathod return filterd values
 
 let odd = arr.filter(val => {
-    if(val % 2 != 0){
-        return val ;
-    } 
+    if (val % 2 != 0) {
+        return val;
+    }
 });
 
 console.log(odd);
