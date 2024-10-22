7ba754ea6888ca142b6ef9e3b0d834d64560986a
SAURAV VERMA <sauravverma9456@gmail.com>
2024-10-22 18:42:00 +0530

    add

--- javasript/36_siman_game/app.js
+++ javasript/36_siman_game/app.js
@@ -4,3 +4,11 @@ btn[1].style.backgroundColor = "yellow";
 btn[2].style.backgroundColor = "green";
 btn[3].style.backgroundColor = "blue";
 
+const fun = () => {
+    btn[0].style.backgroundColor = "white";
+    setTimeout(() => {
+        btn[0].style.backgroundColor = "red";
+    }, 100);
+}
+
+btn[0].addEventListener("click", fun);
