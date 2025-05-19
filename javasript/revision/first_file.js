67eeab3c11b5c0d5b71dcb6b9f54df0835daad0f
Saurav Verma <sauravverma9456@gmail.com>
2025-05-19 23:07:31 +0530

    revsion

--- javasript/revision/first_file.js
+++ javasript/revision/first_file.js
@@ -1,34 +1,29 @@
-// let arr=[[12],[34],[56],[78]];
-// console.log(`2D array : ${arr}`);
-// for(i=0 ; i<arr.length-1 ; i++){
-//     console.log(`arr ${i+1} : `);  // arr[1] is : 
-//     for(j=0 ; j<2 ; j++){
-//         console.log(arr[i][j]);   // 12 
-//     }
-// }
-// let arr = [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]];
-// let index = 0; 
-// let a = 0;
-// for (i of arr) {
-//     console.log(`index ${index} value is : ${i}`)
-//     let b = 0;
-//     for (j of arr[index]) {
-//         console.log(`arr[${a},${b}] value is ${j}`);
-//         b++;
-//     }
-//     index++;
-//     a++;
-// }
-let name = "saurav";
-var age = 23;
-const addres = "bsr";
-console.log(`name is : ${name}`);
-console.log(`name is : ${age}`);
-console.log(`name is : ${addres}`);
-
-let array = [1, 2, 3, 4, 5, 6];
-let index = 0;
-for (let i = 0; i <= array.length - 1; i++) {
-    console.log(`value of i in index[${index}] : ${array[i]}`);
-    index++;
+// all loops in one program 
+// for loop
+console.log(`this is for loop`);
+for (let i = 0; i <= 5; i++) {
+    console.log(`index  ${i}`);
 }
+
+//forOf loop
+console.log(`this is forOf`)
+let str="saurav";
+for(let char of str){
+    console.log(`character ${char}`);
+}
+
+// while loop
+console.log(`this is while loop`);
+let i=0;
+while(i<=5){
+    console.log(`index ${i}`);
+    i++;
+}
+
+// doWhile loop
+let I=0;
+console.log(`this is doWhile loop`);
+do{
+    console.log(`index ${I}`);
+    I++;
+}while(I<=5)
