270920f26085a2b744504c319b5b44172a5994ae
Saurav Verma <sauravverma9456@gmail.com>
2025-08-20 20:56:48 +0530

    minorFommat

--- java/DSA/src/hello_pro.java
+++ java/DSA/src/hello_pro.java
@@ -1,6 +1,6 @@
-public class hello_pro{
-    void main(String[] args)throws Exception {
+public class hello_pro {
+    void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        // ln use for NEXT LINE in java 
+        // ln use for NEXT LINE in java
     }
 }
