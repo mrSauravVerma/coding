7d12236c376e14ea7df0cf8cb543d4c26bca96b4
Saurav Verma <sauravverma9456@gmail.com>
2025-08-25 19:26:22 +0530

    calculate area of circule

---
+++ java/DSA/src/area_of_circule_8.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class area_of_circule_8 {
+    public static void main(String[] args) {
+        System.out.println("enter the radios of circule : ");
+        Scanner sc=new Scanner(System.in);
+        int radius = sc.nextInt();
+        double pai = 3.14;
+        System.out.println("area of circule is :"+(pai*radius*radius));
+    }
+}
