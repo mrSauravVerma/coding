39f780e1f7900533aa7a229a51562bacc17d24f8
Saurav Verma <sauravverma9456@gmail.com>
2025-08-25 18:41:03 +0530

    add with input

--- java/DSA/src/Value_input_6.java
+++ java/DSA/src/Value_input_6.java
@@ -4,6 +4,7 @@ public class Value_input_6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String name = sc.next();
+        // String name = sc.nextLine(); nextLine use for input more than one word
         System.out.println("my name is " + name);
     }
 }
---
+++ java/DSA/src/add_pro_7.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class add_pro_7 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int sum = a+b;
+        System.out.println("the sum of a and b is " + sum);
+    }
+    
+}
