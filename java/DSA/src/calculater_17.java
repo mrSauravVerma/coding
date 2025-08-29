a1b9de9388df0dc46b631d63363ae3cb7a783251
Saurav Verma <sauravverma9456@gmail.com>
2025-08-29 22:13:16 +0530

    make calculator

---
+++ java/DSA/src/calculater_17.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class calculater_17 {
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter a : ");
+    int a = sc.nextInt();
+    System.out.println("enter b : ");
+    int b = sc.nextInt();
+    System.out.println("enter the opreand : ");
+    char op = sc.next().charAt(0);
+    switch(op){
+        case '+':System.out.println("a+b : "+(a+b));
+        break;
+        case '-':System.out.println("a-b : "+(a-b));
+        break;
+        case '*':System.out.println("a*b : "+(a*b));
+        break;
+        case '/':System.out.println("a/b : "+(a/b));
+        break;
+        case '%':System.out.println("a%b : "+(a%b));
+        break;
+        default:System.out.println("invalid opreand");
+    sc.close();
+    }
+    }
+}
--- java/DSA/src/largest_of_3num_16.java
+++ java/DSA/src/largest_of_3num_16.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class average_of_3num_16 {
+public class largest_of_3num_16 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter 3 numbers : ");
