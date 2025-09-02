ffcb269d3e6d0f6065f0c46955a383da5c56bcf9
Saurav Verma <sauravverma9456@gmail.com>
2025-09-02 20:54:26 +0530

    function in java

---
+++ java/java_Basics/src/function/return_fun_3.java
@@ -0,0 +1,20 @@
+package function;
+
+import java.util.*;
+
+public class return_fun_3 {
+
+    public static int calculate_square(int n){
+        int cal = n*n;
+        return cal;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the any number :");
+        int n = sc.nextInt();
+        int square;
+        square = calculate_square(n);
+        System.out.print("square of "+n+" :"+square);
+        sc.close();
+    }
+}
