b42e018908be9a91c0181b789f2953182f1d066a
Saurav Verma <sauravverma9456@gmail.com>
2025-09-08 00:00:10 +0530

    array

--- java/java_Basics/src/array/Declaration_arr_1.java
+++ java/java_Basics/src/array/Declaration_arr_1.java
@@ -7,6 +7,7 @@ public class Declaration_arr_1 {
         arr[0] = 10;
         arr[1] = 20;    
         arr[2] = 30;
+        // int[] arr = {10, 20, 30}; // Alternative way to declare and initialize an array
 
         // Accessing and printing array elements
         for (int i = 0; i < arr.length; i++) {
