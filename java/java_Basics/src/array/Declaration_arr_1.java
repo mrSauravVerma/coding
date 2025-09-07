29440957c7bafdecf17a1be657a58a50e8c425ab
Saurav Verma <sauravverma9456@gmail.com>
2025-09-07 23:58:16 +0530

    array declaration in java

---
+++ java/java_Basics/src/array/Declaration_arr_1.java
@@ -0,0 +1,16 @@
+package array;
+
+public class Declaration_arr_1 {
+    public static void main(String[] args) {
+        // Declaration and initialization of an array
+        int[] arr =new  int[3];
+        arr[0] = 10;
+        arr[1] = 20;    
+        arr[2] = 30;
+
+        // Accessing and printing array elements
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Element at index " + i + ": " + arr[i]);
+        }
+    }
+}
