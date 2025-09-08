1e6a141fe147cd87c9388466ed33baf0302ed48b
Saurav Verma <sauravverma9456@gmail.com>
2025-09-08 22:11:38 +0530

    inptu array in run time in java

--- java/java_Basics/src/array/Declaration_arr_1.java
+++ java/java_Basics/src/array/Declaration_arr_1.java
@@ -7,8 +7,12 @@ public class Declaration_arr_1 {
         arr[0] = 10;
         arr[1] = 20;    
         arr[2] = 30;
-        // int[] arr = {10, 20, 30}; // Alternative way to declare and initialize an array
+        /*int[] arr = {10, 20, 30};
+        Alternative way(shortcut) to declare and initialize an array
 
+        int arr2[] = new int[]{10,20};
+        Another way to declare and initialize an array */
+                                                        
         // Accessing and printing array elements
         for (int i = 0; i < arr.length; i++) {
             System.out.println("Element at index " + i + ": " + arr[i]);
---
+++ java/java_Basics/src/array/runTime_inputARR.java
@@ -0,0 +1,31 @@
+package array;
+// run time input of array program
+import java.util.*;
+
+public class runTime_inputARR {
+
+    public static void forInputFun(int s, int arr[]) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < s; i++) {
+            System.out.print("enter the array" + "[" + i + "]" + " : ");
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+    }
+
+    public static void printArray(int s, int arr[]) {
+        for (int i = 0; i < s; i++) {
+            System.out.println("arr[" + i + "]" + ": " + arr[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the size of array : ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        forInputFun(size, arr);
+        printArray(size, arr);
+        sc.close();
+    }
+}
