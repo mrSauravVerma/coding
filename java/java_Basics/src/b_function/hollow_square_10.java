9b6c157f0ef9e0a9ac2fc1c31622389ddea29cb7
Saurav Verma <sauravverma9456@gmail.com>
2025-09-14 23:15:51 +0530

    print hollow patter

---
+++ java/java_Basics/src/b_function/hollow_square_10.java
@@ -0,0 +1,32 @@
+package b_function;
+
+// Write a program to print hollow square pattern of * using function.
+import java.util.*;
+
+public class hollow_square_10 {
+
+    public static void printPattern(int raw, int col) {
+        for (int i = 1; i <= raw; i++) {
+            for (int j = 1; j <= col; j++) {
+                if (i == 1 || i == raw || j == 1 || j == col) {
+                    // i==1 -> first row, i==raw -> last row
+                    // j==1 -> first column, j==col -> last column
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] agrs) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the number of rows: ");
+        int raw = sc.nextInt();
+        System.out.print("Enter the number of column: ");
+        int col = sc.nextInt();
+        sc.close();
+        printPattern(raw, col);
+    }
+}
