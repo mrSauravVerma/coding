fd72275f945d661b5875142bc1c68000f10377ea
Saurav Verma <sauravverma9456@gmail.com>
2025-09-17 18:10:53 +0530

    add patter in half_pramid pro

--- java/java_Basics/src/b_function/half_pramid.java
+++ java/java_Basics/src/b_function/half_pramid.java
@@ -1,18 +1,39 @@
 package b_function;
 
 public class half_pramid {
-    public static void main(String[] args) {
-        System.out.print("inverted and rotated helf-pramid");
-        for(int i=5 ; i>=0 ; i--){
-            for(int j=0 ; j<=5 ; j++){
-                if(j<i){
+
+    public static void star_half_pramid(int n) {
+        System.out.print("inverted and rotated star printed helf-pramid");
+        for (int i = n; i >= 0; i--) {
+            for (int j = 0; j <= n; j++) {
+                if (j < i) {
                     System.out.print(" ");
-                }
-                else if(j>i){
+                } else if (j > i) {
                     System.out.print("*");
                 }
             }
             System.out.println();
         }
+    }
+
+    public static void number_half_pramid(int n) {
+        System.out.print("inverted and rotated number printed helf-pramid\n");
+        int N = n;
+        for (int i = 1; i <= N; i++) {
+          for (int j = 1; j <= N - 1; j++){
+                System.out.print(" ");
+            }
+            for (int j = N - 1; j < n; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+            --N;
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        star_half_pramid(n);
+        number_half_pramid(n);
     }
 }
