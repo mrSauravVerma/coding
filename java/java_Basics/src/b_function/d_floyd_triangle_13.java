12efda58ef761790704ff17fcae0a7fb3e6feb29
Saurav Verma <sauravverma9456@gmail.com>
2025-09-18 21:48:25 +0530

    floyd triangle & java extension problem

--- java/java_Basics/src/b_function/d_floyd_triangle_13.java
+++ java/java_Basics/src/b_function/d_floyd_triangle_13.java
@@ -5,12 +5,19 @@ import java.util.*;
 public class d_floyd_triangle_13 {
 
     public static void floyd_triangle(int n) {
-
+        int num = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num + " ");
+                num++;
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number of Floyd's triangle line : ");
+        System.out.print("Enter the line of Floyd's triangle :");
         int n = sc.nextInt();
         sc.close();
         floyd_triangle(n);
