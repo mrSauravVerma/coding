4c8db247f24ea6b01bfd415a997a0d5a652cfd43
Saurav Verma <sauravverma9456@gmail.com>
2025-09-21 18:56:06 +0530

    change linear search program

--- java/java_Basics/src/c_array/d_linear_search.java
+++ java/java_Basics/src/c_array/d_linear_search.java
@@ -1,20 +1,25 @@
 package c_array;
 
+import java.util.*;
+
 public class d_linear_search {
 
     public static void linearSearch(int arr[], int num) {
         for (int i = 0; i < 10; i++) {
             if (arr[i] == num) {
-                System.out.print("index of " + num + ": " + i);
-                
+                System.out.print("index of " + num + " is : " + (i + 1));
+
             }
         }
     }
 
     public static void main(String[] args) {
-        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        int num = 6;
-        System.out.print("find index of 6 \n");
+        int arr[] = { 2, 7, 4, 9, 5, 1, 3, 10, 6, 8 };
+        System.out.print("Enter the number between 1 to 10 : ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        sc.close();
+        System.out.print("find index of " + num + "\n");
         linearSearch(arr, num);
     }
 }
