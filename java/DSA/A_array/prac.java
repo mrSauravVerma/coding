3705ad7c70361c9c71663a11a1516c35f8dcea90
Saurav Verma <sauravverma9456@gmail.com>
2025-09-28 18:37:57 +0530

    practic only

---
+++ java/DSA/A_array/prac.java
@@ -0,0 +1,12 @@
+public class prac {
+
+    public static void print(int arr[]){
+        for(int i=0 ; i<arr.length ; i++){
+            System.out.println("intdex["+i+"] : "+arr[i]);
+        }
+    }
+    public static void main(){
+        int [] array = {4,3,2,1};
+        print(array);
+    }
+}
