4b8183af37a7b3247da403420ddf106eb8e2514d
Saurav Verma <sauravverma9456@gmail.com>
2025-09-28 19:56:49 +0530

    satup c&C++ and write some pro

---
+++ c language/NIET_PRO/b_add_2_num.c
@@ -0,0 +1,9 @@
+#include<Stdio.h>
+#include<conio.h>
+void main(){
+    int a,b,c;
+    a=2;
+    b=3;
+    c=a+b;
+    printf("%d + %d : %d",a,b,c);
+}
---
+++ c language/NIET_PRO/c_input_value.c
@@ -0,0 +1,10 @@
+#include <Stdio.h>
+#include <conio.h>
+void main()
+{
+    int a, b, c;
+    printf("Enter the value of a & b:");
+    scanf("%d%d", &a, &b);
+    c = a + b;
+    printf("%d + %d : %d", a, b, c);
+}
