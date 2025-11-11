e480292dcb92e25debb02844fb0180b4ef848c3d
Saurav Verma <sauravverma9456@gmail.com>
2025-11-11 20:58:31 +0530

    stataic program in c

---
+++ c language/NIET_PRO/collage_pro/d_static_var.c
@@ -0,0 +1,20 @@
+// Count use for give a fix value for any variable
+// 📍 Keeps the variable’s value even after the function ends.
+#include <stdio.h>
+#include <conio.h>
+void fun_count();
+
+void fun_count()
+{
+    int count = 0;
+    count++; // static variable
+    printf("value of count in 1st function : %d \n", count);
+}
+
+int main()
+{
+    fun_count();
+    fun_count();
+    fun_count();
+    return 0;
+}
