63108e47a7107c41c01e7cfd10cc32515b69fea2
Saurav Verma <sauravverma9456@gmail.com>
2025-12-25 22:31:53 +0530

    pointer pro 1

--- c language/NIET_PRO/collage_pro/pointer/simple_pro.c
+++ c language/NIET_PRO/collage_pro/pointer/simple_pro.c
@@ -3,6 +3,10 @@ int main(){
     int a=10;
     int *p;
     p=&a;
-    printf("value of a is %d\n",a);
+    printf("value of a is %d\n",a);   //value of a is 10
+    printf("value of *p is %d\n",*p);  //value of *p is 10
+    printf("value of p is %p\n",p);  //value of p is 0061FF1C
+    printf("address of a is %p\n",&a);  //address of a is 0061FF1C
+    printf("address of p is %p\n",&p);  //address of p is 0061FF18
     return 0;
 }
