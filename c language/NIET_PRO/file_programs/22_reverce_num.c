ca430eb721adafac0dbc016a4ea1f29ef5aaeeab
Saurav Verma <sauravverma9456@gmail.com>
2026-01-06 20:49:31 +0530

    paper practic

--- c language/NIET_PRO/file_programs/22_reverce_num.c
+++ c language/NIET_PRO/file_programs/22_reverce_num.c
@@ -1,5 +1,15 @@
-#include<stdio.h>
-int main(){
-    int num;
+#include <stdio.h>
+int main()
+{
+    int num, last_n, r_num = 0;
+    printf("Enter a number : ");
+    scanf("%d", &num);
+    while (num != 0)
+    {
+        last_n = num % 10;
+        r_num = r_num * 10 + last_n;
+        num = num / 10;
+    }
+    printf("reverce number : %d", r_num);
     return 0;
 }
---
+++ c language/NIET_PRO/file_programs/24_average_num.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n1, n2, n3, n4, n5;
+    float avg;
+    printf("enter five number : ");
+    scanf("%d", &n1);
+    scanf("%d", &n2);
+    scanf("%d", &n3);
+    scanf("%d", &n4);
+    scanf("%d", &n5);
+    avg = (n1 + n2 + n3 + n4 + n5) / 5;
+    printf("average of numbers : %f", avg);
+    return 0;
+}
