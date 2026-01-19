bb97fee9e96526ebb0d73a7542f853feff54abab
Saurav Verma <sauravverma9456@gmail.com>
2026-01-19 22:59:27 +0530

    fix small bug

--- java/java_Basics/src/a_basics/a_hello_pro_1.java
+++ java/java_Basics/src/a_basics/a_hello_pro_1.java
@@ -1,6 +1,6 @@
 package a_basics;
 public class a_hello_pro_1 {
-    void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         // ln use for NEXT LINE in java
     }
--- java/java_Basics/src/a_basics/b_print_pattern_2.java
+++ java/java_Basics/src/a_basics/b_print_pattern_2.java
@@ -1,7 +1,7 @@
 package a_basics;
 public class b_print_pattern_2 {
 
-    void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         System.out.println("*****");
         System.out.println("****");
         System.out.println("***");
