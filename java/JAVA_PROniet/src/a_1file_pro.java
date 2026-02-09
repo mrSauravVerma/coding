88711aff312396070bb28888f5760b667b911d18
SAURAV VERMA <sauravverma9456@gmail.com>
2026-02-09 11:27:30 +0530

    Add Java program to count array elements
    Implement a Java program to count elements in an array.

---
+++ java/JAVA_PROniet/src/a_1file_pro.java
@@ -0,0 +1,13 @@
+// DSA programs
+// program 1
+
+class Main {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        int count=0;
+        for(int i=0 ; i<arr.length ; i++){
+            count++;
+        }
+        System.out.print("maximum element in array is :" + count);
+    }
+}
