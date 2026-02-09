f15d08cf3c14b0a67691eb29f817cd6d835b6e0a
SAURAV VERMA <sauravverma9456@gmail.com>
2026-02-09 11:40:39 +0530

    reverce array print

---
+++ java/JAVA_PROniet/src/C_file_pro.java
@@ -0,0 +1,12 @@
+// DSA programs
+// program 3
+
+class Main {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        System.out.println("Reverce of array");
+        for(int i=(arr.length)-1 ; i>=0 ; i--){
+        System.out.print(arr[i]+" ");
+        }
+    }
+}
