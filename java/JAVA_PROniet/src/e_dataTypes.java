3e7c62fb3052d6c6c789eba17af404cfe379ad2b
Saurav Verma <sauravverma9456@gmail.com>
2026-02-23 09:36:27 +0530

    ----------

--- java/JAVA_PROniet/src/e_dataTypes.java
+++ java/JAVA_PROniet/src/e_dataTypes.java
@@ -4,14 +4,32 @@ public class e_dataTypes {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num;     // 5 integer limit is -2 billion to +2 billion
+        // int size is 4 byte (32 bit) and it can store whole number without decimal point
+
         char ch;     // 'a'
+        // char size is 2 byte (16 bit) and it can store single character or symbol or digit
+
         String str;  // "saurav verma"
+        // string size is not fixed and it can store sequence of characters, symbols, digits, space etc.
+
         float fnum;  // 5.5f float limit is up to 7 decimal digits
+        // float size is 4 byte (32 bit) and it can store fractional numbers with decimal point
+
         boolean tf;  // true false
+        // boolean size is 1 byte (8 bit) and it can store only two values: true or false
+
         double ffnum;// 43.76 double limit is up to 15 decimal digits
+        // double size is 8 byte (64 bit) and it can store fractional numbers with decimal point with higher precision than float
+
         long lnum;   // 123456789L long limit is -9 quintillion to +9 quintillion
+        // long size is 8 byte (64 bit) and it can store whole number without decimal point with a wider range than int
+
         byte by;     // 127 byte limit is -128 to 127
+        // byte size is 1 byte (8 bit) and it can store whole number without decimal point with a smaller range than int
+
         short sh;    // 1000 short limit is -32,768 to 32,767
+        // short size is 2 byte (16 bit) and it can store whole number without decimal point with a smaller range than int
+        
         System.out.print("Enter Interger number : ");
         num = sc.nextInt();
         
