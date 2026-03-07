2f71a1eba8101208bcefc2b446a8cffd5b5fd24d
Saurav Verma <sauravverma9456@gmail.com>
2026-03-07 10:51:44 +0530

    obj and classes in java

---
+++ java/java_advance/src/A_classes_obj.java
@@ -0,0 +1,26 @@
+class A_classes_obj {
+
+    public void show_value(int n){
+        System.out.print("value is : " + n);
+    }
+
+    public static void main(String args[]){
+        A_classes_obj obj = new A_classes_obj();
+        int num = 10;
+        obj.show_value(num);
+    }
+}
+
+// with the of static keyword we do function call directly 
+// without creating object of the class but without static keyword we have to create object of the class to call the function
+// class A_classes_obj {
+
+//     public static void show_value(int n){
+//         System.out.print("value is : " + n);
+//     }
+
+//     public static void main(String args[]){
+//         int num = 10;
+//         show_value(num);
+//     }
+// }
