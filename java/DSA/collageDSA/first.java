09c5a9dcfc6f69dcc83214a700000dd8905e101f
Saurav Verma <sauravverma9456@gmail.com>
2026-03-13 20:41:16 +0530

    practice

---
+++ java/DSA/collageDSA/first.java
@@ -0,0 +1,14 @@
+//this code is correct but not run in vs code
+// manually compile and run this code in terminal
+
+class first {
+
+    first() {
+        System.out.println("constructor method.");
+    }
+
+    public static void main(String[] args) {
+        first ob = new first();
+    }
+
+}   
