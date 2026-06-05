7d0486f02860fc17e2900d45a9a18fabf1aebf0f
Saurav Verma <sauravverma9456@gmail.com>
2026-06-05 23:08:21 +0530

    day 2 compleat

--- java/DSA/restart/c_array.java
+++ java/DSA/restart/c_array.java
@@ -6,14 +6,32 @@ public class c_array {
 
         System.out.print("Enter the size of array : ");
         int n = sc.nextInt();
-        int[] arr = new int[4];
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
-        System.out.print("Enter arr[" + i + "] : ");
+            System.out.print("Enter arr[" + i + "] : ");
             arr[i] = sc.nextInt();
         }
         for (int i = 0; i < n; i++) {
             System.out.println("arr[" + i + "] : " + arr[i]);
         }
+        int gNum = arr[0];
+        int sNum = arr[0];
+        int sum = arr[0];
+        for (int i = 1; i < n; i++) {
+            if(arr[i]>gNum){
+                gNum = arr[i];
+            }
+            if(arr[i]<sNum){
+                sNum = arr[i];
+            }
+            sum = sum + arr[i];
+        }
+
+        float avg = sum/n;
+        System.out.println("Greatest number in array is : " + gNum);
+        System.out.println("Smallest number in array is : " + sNum);
+        System.out.println("Sum of all array numbers : " + sum);
+        System.out.print("Avarage of all number : " + avg);
 
         sc.close();
     }
