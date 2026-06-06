af04f269220eed69560dc2d02b33fef1086a521b
Saurav Verma <sauravverma9456@gmail.com>
2026-06-06 20:06:06 +0530

    method in java day 3

---
+++ java/DSA/restart/d_methods.java
@@ -0,0 +1,17 @@
+public class d_methods {
+
+    public static void addtion(int a, int b) {
+        System.out.println("a+b : " + (a + b));
+    }
+    public static int subtraction(int x , int y)
+    {
+        return (x-y);
+    }
+    public static void main(String args[]) {
+        int a, b;
+        a = 23;
+        b = 24;
+        addtion(a, b);
+        System.out.println("a-b : " + subtraction(a, b));
+    }
+}
