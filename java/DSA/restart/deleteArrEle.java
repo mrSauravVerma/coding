6bc4476fabe1a88bd697836dd122ebd12baa70fb
SAURAV VERMA <sauravverma9456@gmail.com>
2026-06-17 11:41:11 +0530

    Add deleteArrEle.java to remove array elements
    Implement a Java program to delete an element from an array.

---
+++ java/DSA/restart/deleteArrEle.java
@@ -0,0 +1,16 @@
+class deleteArrEle {
+    public static void main(String[] args) {
+        
+        int arr[] = {1,2,3,4,5};
+        
+        int pos = 2; // arr[1]
+        
+        for(int i = (pos - 1) ; i < arr.length-1 ; i++){
+            arr[i] = arr[i+1];
+        }
+        
+        for(int i = 0 ; i < arr.length-1 ; i++){
+            System.out.println("arr[" + i + "] : " + arr[i]);
+        }
+    }
+}
