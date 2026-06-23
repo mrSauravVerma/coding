a33b142b970c56ce10b4193866382fa3924a52c9
SAURAV VERMA <sauravverma9456@gmail.com>
2026-06-23 10:14:04 +0530

    Add minMaxArray class with max and min methods
    This class includes methods to find the maximum and minimum values in an array.

---
+++ java/DSA/restart/minMaxArray.java
@@ -0,0 +1,33 @@
+class calValue {
+    public void maxNum(int[] arr) {
+        int maxV = Integer.MIN_VALUE; // mimimum value in integers values in java
+        for (int i = 0; i < arr.length; i++) {
+            if (maxV < arr[i]) {
+                maxV = arr[i];
+            }
+        }
+        System.out.println("Maximum vlaue in array : " + maxV);
+    }
+
+    public void minNum(int[] arr) {
+        int minV = Integer.MAX_VALUE; // maximum value in integers values in java
+        for (int i = 0; i < arr.length; i++) {
+            if (minV > arr[i]) {
+                minV = arr[i];
+            }
+        }
+        System.out.println("Maximum vlaue in array : " + minV);
+    }
+}
+
+public class minMaxArray {
+
+    public static void main(String[] args) {
+
+        int arr[] = { 1, 2, 6, 4, 2, 63, 23 };
+
+        calValue obj = new calValue();
+        obj.maxNum(arr);
+        obj.minNum(arr);
+    }
+}
