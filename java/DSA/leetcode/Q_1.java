450214b9e6cafb466632de51841271ed6de26075
Saurav Verma <sauravverma9456@gmail.com>
2026-08-08 09:48:57 +0530

    ----------

--- java/DSA/leetcode/Q_1.java
+++ java/DSA/leetcode/Q_1.java
@@ -12,14 +12,27 @@
 //     }
 // }
 
-// public class Q_1 {
-//     public static void main(String args[]) {
-//         Solution obj = new Solution();
+class Solution {
+    public int[] twoSum(int[] arr, int t) {
+        int i = 0, j = arr.length - 1;
+        while (i < arr.length / 2) {
+            if (arr[i] + arr[j] == t) {
+                return new int[] { i, j };
+            }
+            i++;
+            j--;
+        }
+        return new int[] {0,0};
+    }
+}
 
-//         int[] arr = { 1, 2, 3, 4, 5 };
-//         int target = 9;
-//         int[] result =obj.twoSum(arr, target);
-//         System.out.print("sum of 9 on indexs is : " + result[0] + " and " + result[1]);
-//     }
-// }
- 
+public class Q_1 {
+    public static void main(String args[]) {
+        Solution obj = new Solution();
+
+        int[] arr = { 1, 2, 3, 4, 5 };
+        int target = 9;
+        int[] result = obj.twoSum(arr, target);
+        System.out.print("sum of 9 on indexs is : " + result[0] + " and " + result[1]);
+    }
+}
