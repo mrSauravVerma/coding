702930f5bc05532c553953a4bbea630b2736ffba
Saurav Verma <sauravverma9456@gmail.com>
2026-09-21 11:14:44 +0530

    addition with pointer

--- java/DSA/restart/n_two_pointer.java
+++ java/DSA/restart/n_two_pointer.java
@@ -1,3 +1,26 @@
 public class n_two_pointer {
-    
+    static int[] sol(int[] arr, int targate) {
+        int tail = 0;
+        int head = arr.length - 1;
+
+        while (tail < head) {
+            int sum = arr[tail] + arr[head];
+
+            if (sum == targate) {
+                return new int[] { tail, head };
+            }
+            if (sum > targate)
+                head--;
+            else
+                tail++;
+        }
+        return new int[] { -1, -1 };
+    }
+
+    public static void main(String[] arga) {
+        int[] arr = { 1, 4, 6, 7, 19 };
+        int targate = 13;
+        int[] result = sol(arr, targate);
+        System.out.print("Indexes : " + result[0] + "," + result[1]);
+    }
 }
